f4e875ff7d014e6677df4ea39c510cc1decfb4e1
Kanonidism <162639922+Kanonidism@users.noreply.github.com>
2024-04-02 23:44:00 +0300

    bot.js

--- bot.js
+++ bot.js
@@ -139,6 +139,6 @@ printWatermark();
 
    # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :   ]
+   ## YT : 
 */
